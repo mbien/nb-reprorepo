995e19d07d4d455c2306cf8677984e61d1e89814
Michael Bien <mbien42@gmail.com>
2025-02-11 22:15:50 +0100

    javadoc formatting issues caused by refactoring

---
+++ tinybugs/src/main/java/bugs/javadocformatting/RefactoringInfluencesJavadocFormatting.java
@@ -0,0 +1,29 @@
+package bugs.javadocformatting;
+
+// regression since NB 24+
+// issue https://github.com/apache/netbeans/issues/8233
+
+/**
+ * This is the class comment.
+ * It references {@link #addParamHint(int) }.
+ * It also references {@link #renameMe() }.
+ */
+public class RefactoringInfluencesJavadocFormatting {
+
+    /**
+     * This is another example.
+     * It references {@link #renameMe() }.
+     */
+    public void addParamHint(int param) {
+//                           ^^^ add-param hint should not reformat doc
+    }
+
+    /**
+     * This is another example.
+     * It references {@link #m1() }.
+     */
+    public void renameMe() {
+//              ^^^^rename should not reformat doc
+    }
+    
+}
