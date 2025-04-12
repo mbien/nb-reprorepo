a394090b356ab6ec2eecee1c7b0612dd509c6fe7
Michael Bien <mbien42@gmail.com>
2025-04-13 01:47:24 +0200

    benchmark project with 10k inner classes.
     - useful to debug hints, navigator and completion performance

---
+++ performance/classes/src/main/java/test/classes/Reset.java
@@ -0,0 +1,12 @@
+package test.classes;
+
+/**
+ * based on https://bz.apache.org/netbeans/show_bug.cgi?id=194816
+ */
+public class Reset {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
