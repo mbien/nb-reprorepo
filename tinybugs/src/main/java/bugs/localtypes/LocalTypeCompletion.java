54003b76bc25116e2f563940836709f02d125b19
Michael Bien <mbien42@gmail.com>
2025-04-20 22:00:54 +0200

    Local type completion issues.

---
+++ tinybugs/src/main/java/bugs/localtypes/LocalTypeCompletion.java
@@ -0,0 +1,21 @@
+package bugs.localtypes;
+
+// https://github.com/apache/netbeans/issues/8436
+public class LocalTypeCompletion {
+
+    private record Item1(String name) {}
+    private class Item2{} {}
+    private enum Item3{} {}
+
+    public void test() {
+        
+        record Item4(String name) {}
+        class Item5{} {}
+        enum Item6{} {}
+
+//        new Item
+        // first completion contains all 6 Items,
+        // hitting ctrl+space again doesn't contain method-local Items
+        // as concequence, constructor completion does also not work.
+    }
+}
